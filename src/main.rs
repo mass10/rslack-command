b5afeefc8c42fe37aa19814ca2704cb716b86826
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-06-21 09:21:32 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ struct StartConfigurationSettings {
 	/// Path to settings.toml.
 	settings_path: String,
 
-	/// Task name to launch.
-	target_task: String,
+	/// Name of the tasks to launch.
+	target_tasks: Vec<String>,
 }
 
 impl StartConfigurationSettings {
@@ -26,80 +26,22 @@ impl StartConfigurationSettings {
 	/// A new instance of `StartConfigurationSettings`.
 	pub fn new() -> std::result::Result<StartConfigurationSettings, Box<dyn std::error::Error>> {
 		let mut instance = StartConfigurationSettings {
-			target_task: String::new(),
+			target_tasks: vec![],
 			settings_path: String::new(),
 		};
-		if true {
-			instance.configure()?;
-		}
-		if false {
-			instance.configure2()?;
-		}
+		instance.configure()?;
 		return Ok(instance);
 	}
 
-	/// Reads commandline options
-	fn configure(&mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
-		let mut current_option = String::new();
-
-		// Commandline options. The 1st token is application itself.
-		let args: Vec<String> = std::env::args().skip(1).collect();
-
-		// Reading tokens
-		for e in args {
-			if e == "--help" || e == "-h" {
-				return Err("show usage".into());
-			}
-			if e == "--version" || e == "-v" {
-				return Err("show version".into());
-			}
-			if e.starts_with("--file=") || e.starts_with("-f=") {
-				let (_, value) = util::functions::split_string(&e, "=");
-				if value == "" {
-					return Err("show usage".into());
-				}
-				self.settings_path = value;
-				continue;
-			}
-			if e == "--file" || e == "-f" {
-				current_option = e;
-				continue;
-			}
-			if e.starts_with("-") {
-				// Unknown option flag given.
-				current_option.clear();
-				println!("Unknown option {}", e);
-				return Err("show usage".into());
-			}
-
-			if current_option == "--file" || current_option == "-f" {
-				// Must be the path to rmake file.
-				self.settings_path = e;
-				current_option.clear();
-				continue;
-			}
-
-			// Must be the name of a task to launch.
-			self.target_task = e;
-		}
-
-		if current_option != "" {
-			// No values followed option flag.
-			return Err("show usage".into());
-		}
-
-		// Configuration valid.
-		return Ok(());
-	}
-
-	fn configure2(&mut self) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
+	/// Configures the instance.
+	fn configure(&mut self) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
 		// Options to be served.
 		let mut getopt = getopts::Options::new();
-		getopt.optflag("h", "help", "");
-		getopt.optopt("s", "settings", "", "");
-		getopt.optopt("t", "task", "", "");
+		getopt.optflag("h", "help", "Shows usage.");
+		getopt.opt("v", "version", "Shows version info.", "STRING", getopts::HasArg::No, getopts::Occur::Optional);
+		getopt.opt("", "file", "Path to settings.toml", "FILE", getopts::HasArg::Yes, getopts::Occur::Optional);
 
-		// parsing.
+		// parse.
 		let result = getopt.parse(std::env::args().skip(1));
 		if result.is_err() {
 			println!("{}", result.err().unwrap());
@@ -108,21 +50,27 @@ impl StartConfigurationSettings {
 		}
 		let result = result.unwrap();
 
-		// help
+		// optional: help
 		if result.opt_present("help") {
 			println!("{}", getopt.usage(""));
 			return Err("".into());
 		}
 
-		// server
-		if result.opt_present("settings") {
-			self.settings_path = result.opt_str("settings").unwrap();
+		// optional: Path to settings.toml
+		if result.opt_present("file") {
+			self.settings_path = result.opt_str("file").unwrap();
 		}
 
-		// task
-		if result.opt_present("task") {
-			self.target_task = result.opt_str("task").unwrap();
+		// optional: version
+		if result.opt_present("version") {
+			return Err("SHOW_VERSION".into());
 		}
+
+		// required: target tasks
+		if result.free.len() == 0 {
+			return Err("SHOW_USAGE".into());
+		}
+		self.target_tasks = result.free;
 
 		return Ok(());
 	}
@@ -144,21 +92,22 @@ fn version() {
 	println!("{}", "https://github.com/mass10/rslack-command");
 }
 
-/// アプリケーションのエントリーポイント
+/// Rust application entrypoint.
 fn main() {
-	// コマンドラインオプション
+	// Command line arguments.
 	let result = StartConfigurationSettings::new();
 	if result.is_err() {
 		let error = result.err().unwrap();
 		let error_message = error.to_string();
 		if error_message == "" {
 			// NOP
-		} else if error_message == "show usage" {
+		} else if error_message == "SHOW_USAGE" {
 			usage();
-		} else if error_message == "show version" {
+		} else if error_message == "SHOW_VERSION" {
 			version();
 		} else {
-			println!("[ERROR] Unknown error {:?}", &error_message)
+			println!("[ERROR] Unknown error {:?}", &error_message);
+			std::process::exit(1);
 		}
 		return;
 	}
@@ -172,11 +121,8 @@ fn main() {
 	}
 	let application = result.unwrap();
 
-	// Task to launch.
-	let requested_task = conf.target_task;
-
-	// アプリケーションを実行します。
-	let result = application.run(&requested_task);
+	// Run.
+	let result = application.run(&conf.target_tasks);
 	if result.is_err() {
 		println!("[ERROR] {}", result.err().unwrap());
 		return;
